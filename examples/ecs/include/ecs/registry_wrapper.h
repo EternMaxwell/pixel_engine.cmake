7dae06863d2fa9f9c7b89b3ccd47bdaf4dc3d162
Etern Maxwell <maxwell_dev@outlook.com>
2024-05-22 18:50:20 +0800

    systems know work as expected, more features will be added

--- examples/ecs/include/ecs/registry_wrapper.h
+++ examples/ecs/include/ecs/registry_wrapper.h
@@ -2,10 +2,21 @@
 
 #include <entt/entity/registry.hpp>
 #include <map>
+#include <memory>
 #include <set>
 #include <tuple>
 
 namespace ecs_trial {
+    template <template <typename...> typename Template, typename T>
+    struct is_template_of {
+        static const bool value = false;
+    };
+
+    template <template <typename...> typename Template, typename... Args>
+    struct is_template_of<Template, Template<Args...>> {
+        static const bool value = true;
+    };
+
     class Command {
        private:
         entt::registry& m_registry;
@@ -31,9 +42,29 @@ namespace ecs_trial {
             return entity;
         }
 
-        auto& registry() { return m_registry; }
-
         void end() {}
+    };
+
+    template <typename Que, typename Exclude>
+    class Query {};
+
+    template <typename... Qus, typename... Exs>
+    class Query<std::tuple<Qus...>, std::tuple<Exs...>> {
+       private:
+        entt::registry& registry;
+
+       public:
+        Query(entt::registry& registry) : registry(registry) {}
+
+        auto iter() {
+            return registry.view<Qus...>(entt::exclude_t<Exs...>{}).each();
+        }
+
+        auto single() {
+            return *(registry.view<Qus...>(entt::exclude_t<Exs...>{})
+                        .each()
+                        .begin());
+        }
     };
 
     class App {
@@ -41,6 +72,44 @@ namespace ecs_trial {
         entt::registry m_registry;
         std::unordered_map<entt::entity, std::set<entt::entity>>
             m_entity_relation_tree;
+
+        template <typename... Args, typename Tuple, std::size_t... I>
+        void process(void (*func)(Args...), Tuple const& tuple,
+                     std::index_sequence<I...>) {
+            func(std::get<I>(tuple)...);
+        }
+
+        template <typename... Args, typename Tuple>
+        void process(void (*func)(Args...), Tuple const& tuple) {
+            process(func, tuple,
+                    std::make_index_sequence<std::tuple_size<Tuple>::value>());
+        }
+
+        /*!
+         * @brief This is where the systems get their parameters by type.
+         * All possible type should be handled here.
+         */
+        template <typename T, typename... Args>
+        std::tuple<T, Args...> get_values() {
+            static_assert(std::same_as<T, Command> || is_template_of<Query, T>::value,
+                          "an illegal argument type is used in systems.");
+            if constexpr (std::same_as<T, Command>) {
+                if constexpr (sizeof...(Args) > 0) {
+                    return std::tuple_cat(std::make_tuple(command()),
+                                          get_values<Args...>());
+                } else {
+                    return std::make_tuple(command());
+                }
+            } else if constexpr (is_template_of<Query, T>::value) {
+                T query(m_registry);
+                if constexpr (sizeof...(Args) > 0) {
+                    return std::tuple_cat(std::make_tuple(query),
+                                          get_values<Args...>());
+                } else {
+                    return std::make_tuple(query);
+                }
+            }
+        }
 
        public:
         const auto& registry() { return m_registry; }
@@ -57,18 +126,17 @@ namespace ecs_trial {
         template <typename... Args>
         App& run_system(void (*func)(Args...)) {
             using argument_type = std::tuple<Args...>;
-            for (int i = 0; i < std::tuple_size_v<argument_type>; i++) {
-                std::cout << i << std::endl;
-            }
-
-            std::tuple<> value;
-            for (int i = 0; i < sizeof...(Args); i++) {
-                if (std::same_as<std::tuple_element_t<i, argument_type>,
-                                 Command>) {
-                }
-            }
-
+            auto args = get_values<Args...>();
+            process(func, args);
             return *this;
+        }
+
+        template <template <typename...> typename Q, typename... Qus,
+                  typename... Exs>
+        Query<std::tuple<Qus...>, std::tuple<Exs...>> get_query(
+            std::tuple<Qus...> q, std::tuple<Exs...>) {
+            Query<std::tuple<Qus...>, std::tuple<Exs...>> query(m_registry);
+            return query;
         }
 
         void run() {}
--- examples/ecs/src/main.cpp
+++ examples/ecs/src/main.cpp
@@ -1,4 +1,3 @@
-#include <entt/entity/registry.hpp>
 #include <iostream>
 #include <random>
 
@@ -16,11 +15,10 @@ void start(ecs_trial::Command command) {
     for (int i = 0; i < 500; i++) {
         using namespace std;
         int min = 0, max = 100;
-        random_device seed;       // 硬件生成随机数种子
-        ranlux48 engine(seed());  // 利用种子生成随机数引擎
-        uniform_int_distribution<> distrib(
-            min, max);  // 设置随机数范围，并为均匀分布
-        int random = distrib(engine);  // 随机数
+        random_device seed;
+        ranlux48 engine(seed());
+        uniform_int_distribution<> distrib(min, max);
+        int random = distrib(engine);
         if (random > 50) {
             command.spawn(Health{.life = 100.0f}, Position{.x = 0, .y = 0});
         } else {
@@ -29,23 +27,19 @@ void start(ecs_trial::Command command) {
     }
 }
 
-void print(ecs_trial::Command command) {
-    auto view = command.registry().view<Health, Position>();
-    for (auto [entity, health, pos] : view.each()) {
+void print(
+    ecs_trial::Command command,
+    ecs_trial::Query<std::tuple<const Health, const Position>, std::tuple<>>
+        query) {
+    for (auto [entity, health, pos] : query.iter()) {
         std::cout << static_cast<int>(entity) << ": " << health.life
                   << std::endl;
     }
 }
 
-void print2(entt::entity&& entity, Health& health, Position& pos) {
-    std::cout << static_cast<int>(entity) << ": " << health.life << std::endl;
-}
-
 int main() {
     ecs_trial::App app;
-    app.command().spawn(Health{.life = 100.0f});
-    app.run_system_command(start).run_system_command(print);
-    app.run_system(print2);
+    app.run_system(start).run_system(print);
 
     return 0;
 }
