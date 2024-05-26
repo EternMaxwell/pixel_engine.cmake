09188fc9314a02fc5641edc92e4cee58b07a96a8
Etern Maxwell <maxwell_dev@outlook.com>
2024-05-26 22:46:20 +0800

    know query class will not let user access entt::entity until they required it. add struct to test two queries' relation

--- examples/ecs/include/ecs/registry_wrapper.h
+++ examples/ecs/include/ecs/registry_wrapper.h
@@ -23,6 +23,9 @@ namespace ecs_trial {
         static const bool value = true;
     };
 
+    template <typename Q1, typename Q2>
+    struct queries_contrary {};
+
     class EntityCommand {
        private:
         entt::registry* const m_registry;
@@ -163,9 +166,104 @@ namespace ecs_trial {
     class Query<std::tuple<Qus...>, std::tuple<Exs...>> {
        private:
         entt::registry& registry;
+        decltype(registry.view<Qus...>(entt::exclude_t<Exs...>{})
+                     .each()) m_iter;
 
        public:
-        Query(entt::registry& registry) : registry(registry) {}
+        Query(entt::registry& registry) : registry(registry) {
+            m_iter = registry.view<Qus...>(entt::exclude_t<Exs...>{}).each();
+        }
+
+        class iterator : public std::iterator<std::input_iterator_tag,
+                                              std::tuple<Qus...>> {
+           private:
+            decltype(registry.view<Qus...>(entt::exclude_t<Exs...>{})
+                         .each()) m_full;
+            decltype(m_full.begin()) m_iter;
+
+            template <size_t I, typename T, typename... Args>
+            std::tuple<Args...> remove_first(std::tuple<T, Args...> tuple) {
+                if constexpr (sizeof...(Args) > I) {
+                    return std::tuple_cat(std::make_tuple(std::get<I>(tuple)),
+                                          remove_first<I++>(tuple));
+                } else {
+                    return std::make_tuple(std::get<I>(tuple));
+                }
+            }
+
+            template <typename T, typename... Args>
+            std::tuple<Args...> remove_first(std::tuple<T, Args...> tuple) {
+                return remove_first<1>(tuple);
+            }
+
+           public:
+            iterator(decltype(m_full) full)
+                : m_full(full), m_iter(full.begin()) {}
+
+            iterator(decltype(m_full) full, decltype(m_full.begin()) iter)
+                : m_full(full), m_iter(iter) {}
+            iterator& operator++() {
+                m_iter++;
+                return *this;
+            }
+            iterator operator++(int) {
+                iterator tmp = *this;
+                ++(*this);
+                return tmp;
+            }
+            bool operator==(const iterator& rhs) const {
+                return m_iter == rhs.m_iter;
+            }
+            bool operator!=(const iterator& rhs) const {
+                return m_iter != rhs.m_iter;
+            }
+            std::tuple<Qus...> operator*() {
+                // remove the first element of the tuple and return the rest
+                return remove_first(*m_iter);
+            }
+            auto begin() { return iterator(m_full, m_full.begin()); }
+
+            auto end() { return iterator(m_full, m_full.end()); }
+        };
+
+        /*! @brief Get the iterator for the query.
+         * @return The iterator for the query.
+         */
+        auto iter() {
+            return iterator(
+                registry.view<Qus...>(entt::exclude_t<Exs...>{}).each());
+        }
+
+        /*! @brief Get the single entity and requaired components.
+         * @return An optional of a single tuple of entity and requaired
+         * components.
+         */
+        auto single() {
+            auto start = *(registry.view<Qus...>(entt::exclude_t<Exs...>{})
+                               .each()
+                               .begin());
+            if (registry.view<Qus...>(entt::exclude_t<Exs...>{})
+                    .each()
+                    .begin() !=
+                registry.view<Qus...>(entt::exclude_t<Exs...>{}).each().end()) {
+                return std::optional(start);
+            } else {
+                return std::optional<decltype(start)>{};
+            }
+        }
+    };
+
+    template <typename... Qus, typename... Exs>
+    class Query<std::tuple<entt::entity, Qus...>, std::tuple<Exs...>> {
+       private:
+        entt::registry& registry;
+        decltype(registry.view<Qus...>(entt::exclude_t<Exs...>{})
+                     .each()) m_iter;
+
+       public:
+        Query(entt::registry& registry) : registry(registry) {
+            m_iter = registry.view<Qus...>(entt::exclude_t<Exs...>{}).each();
+        }
 
         /*! @brief Get the iterator for the query.
          * @return The iterator for the query.
@@ -190,6 +288,92 @@ namespace ecs_trial {
             } else {
                 return std::optional<decltype(start)>{};
             }
+        }
+    };
+
+    template <typename... Qus1, typename... Qus2, typename... Exs1,
+              typename... Exs2>
+    struct queries_contrary<Query<std::tuple<Qus1...>, std::tuple<Exs1...>>,
+                            Query<std::tuple<Qus2...>, std::tuple<Exs2...>>> {
+        template <typename T, typename... Args>
+        bool args_all_const() {
+            if constexpr (std::is_const_v<T>) {
+                if constexpr (sizeof...(Args) > 0) {
+                    return args_all_const<Args...>();
+                } else {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        template <typename T>
+        static bool type_in_group() {
+            return false;
+        }
+
+        template <typename T, typename First, typename... Args>
+        static bool type_in_group() {
+            if constexpr (std::is_same_v<std::remove_const_t<T>,
+                                         std::remove_const_t<First>>) {
+                return true;
+            } else {
+                if constexpr (sizeof...(Args) > 0) {
+                    return type_in_group<T, Args...>();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        template <typename First, typename Second>
+        struct first_tuple_contains_all_non_const_type_in_second_tuple {
+            static const bool value = false;
+        };
+
+        template <typename... Args1, typename Args2First, typename... Args2>
+        struct first_tuple_contains_all_non_const_type_in_second_tuple<
+            std::tuple<Args1...>, std::tuple<Args2First, Args2...>> {
+            bool value() {
+                if constexpr (sizeof...(Args1) == 0) {
+                    return false;
+                } else if (std::is_const_v<Args2First> ||
+                           type_in_group<Args2First, Args1...>()) {
+                    if constexpr (sizeof...(Args2) > 0) {
+                        return first_tuple_contains_all_non_const_type_in_second_tuple<
+                                   std::tuple<Args1...>, std::tuple<Args2...>>{}
+                            .value();
+                    } else {
+                        return true;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        };
+
+        bool value() {
+            // if Qus1 and Qus2 are all const return false
+            if (args_all_const<Qus1..., Qus2...>()) {
+                return false;
+            }
+
+            // if Exs1 contains all non-const Type in Qus2 return false
+            if (first_tuple_contains_all_non_const_type_in_second_tuple<
+                    std::tuple<Exs2...>, std::tuple<Qus1...>>{}
+                    .value()) {
+                return false;
+            }
+
+            // if Exs2 contains all non-const Type in Qus1 return false
+            if (first_tuple_contains_all_non_const_type_in_second_tuple<
+                    std::tuple<Exs1...>, std::tuple<Qus2...>>{}
+                    .value()) {
+                return false;
+            }
+
+            return true;
         }
     };
 
@@ -280,6 +464,8 @@ namespace ecs_trial {
          */
         void clear() { m_events->clear(); }
     };
+
+    class Scheduler {};
 
     class App {
        private:
--- examples/ecs/src/main.cpp
+++ examples/ecs/src/main.cpp
@@ -32,19 +32,19 @@ void start(ecs_trial::Command command) {
 }
 
 void create_child(ecs_trial::Command command,
-                  ecs_trial::Query<std::tuple<WithChild>, std::tuple<>> query) {
+                  ecs_trial::Query<std::tuple<entt::entity, WithChild>, std::tuple<>> query) {
     std::cout << "create_child" << std::endl;
     for (auto [entity] : query.iter()) {
         std::cout << "entity with child: " << static_cast<int>(entity)
                   << std::endl;
         command.entity(entity).spawn(Health{.life = 50},
-                           Position{.x = 0, .y = 0});
+                                     Position{.x = 0, .y = 0});
     }
     std::cout << std::endl;
 }
 
 void despawn(ecs_trial::Command command,
-             ecs_trial::Query<std::tuple<WithChild>, std::tuple<>> query) {
+             ecs_trial::Query<std::tuple<entt::entity, WithChild>, std::tuple<>> query) {
     std::cout << "despawn" << std::endl;
     for (auto [entity] : query.iter()) {
         command.entity(entity).despawn_recurse();
@@ -59,7 +59,7 @@ void add_res(ecs_trial::Command command) {
 }
 
 void print(
-    ecs_trial::Query<std::tuple<const Health, const Position>, std::tuple<>>
+    ecs_trial::Query<std::tuple<entt::entity, const Health, const Position>, std::tuple<>>
         query) {
     std::cout << "print" << std::endl;
     auto [entity, health, pos] = query.single().value();
@@ -71,6 +71,14 @@ void print(
                   << std::endl;
     }
     std::cout << std::endl;
+}
+
+void access_no_entity_health(
+    ecs_trial::Query<std::tuple<const Health>, std::tuple<>> query) {
+    std::cout << "access_no_entity_health" << std::endl;
+    for (auto [health] : query.iter()) {
+        std::cout << health.life << std::endl;
+    }
 }
 
 void print_res(ecs_trial::Resource<const Health> res) {
@@ -92,15 +100,15 @@ void access_not_exist_res(ecs_trial::Resource<Position> res) {
 }
 
 void add_not_exist_res(ecs_trial::Command command) {
-	std::cout << "add_not_exist_res" << std::endl;
-	command.init_resource<Position>();
-	std::cout << std::endl;
+    std::cout << "add_not_exist_res" << std::endl;
+    command.init_resource<Position>();
+    std::cout << std::endl;
 }
 
 void remove_not_exist_res(ecs_trial::Command command) {
-	std::cout << "remove_not_exist_res" << std::endl;
-	command.remove_resource<Position>();
-	std::cout << std::endl;
+    std::cout << "remove_not_exist_res" << std::endl;
+    command.remove_resource<Position>();
+    std::cout << std::endl;
 }
 
 void change_res(ecs_trial::Resource<Health> res) {
@@ -113,18 +121,18 @@ void change_res(ecs_trial::Resource<Health> res) {
 
 void write_event(ecs_trial::EventWriter<Health> writer) {
     std::cout << "write_event" << std::endl;
-	writer.write(Health{.life = 100.0f});
+    writer.write(Health{.life = 100.0f});
     writer.write(Health{.life = 50.0f});
     writer.write(Health{.life = 10.0f});
     std::cout << std::endl;
 }
 
 void read_event(ecs_trial::EventReader<Health> reader) {
-	std::cout << "read_event" << std::endl;
-	for (auto health : reader.read()) {
-		std::cout << "event: " << health.life << std::endl;
-	}
-	std::cout << std::endl;
+    std::cout << "read_event" << std::endl;
+    for (auto health : reader.read()) {
+        std::cout << "event: " << health.life << std::endl;
+    }
+    std::cout << std::endl;
 }
 
 int main() {
@@ -138,6 +146,15 @@ int main() {
     app.run_system(add_not_exist_res).run_system(access_not_exist_res);
     app.run_system(remove_not_exist_res).run_system(access_not_exist_res);
     app.run_system(write_event).run_system(read_event);
+    app.run_system(access_no_entity_health);
+
+    std::cout
+        << std::boolalpha
+        << ecs_trial::queries_contrary<
+               ecs_trial::Query<std::tuple<const Health>, std::tuple<>>,
+               ecs_trial::Query<std::tuple<Position>, std::tuple<Health>>>()
+               .value()
+        << std::endl;
 
     return 0;
 }
