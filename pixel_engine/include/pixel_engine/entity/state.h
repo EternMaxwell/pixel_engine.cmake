af6cc50aad3a8c837a2c1147cc7b4faaa6f633b0
Etern Maxwell <maxwell_dev@outlook.com>
2024-06-02 14:36:37 +0800

    added state to the app system. like in bevy, the state is added through the resource system

---
+++ pixel_engine/include/pixel_engine/entity/state.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+namespace pixel_engine {
+    namespace entity {
+        template <typename T, std::enable_if<std::is_enum_v<T>>::type* = nullptr>
+        class State {
+           protected:
+            T m_state;
+
+           public:
+            State() : m_state() {}
+            State(const T& state) : m_state(state) {}
+
+            bool is_state(const T& state) { return m_state == state; }
+        };
+
+        template <typename T>
+        class NextState : public State<T> {
+           public:
+            NextState() : State<T>() {}
+            NextState(const T& state) : State<T>(state) {}
+
+            void set_state(const T& state) { State<T>::m_state = state; }
+        };
+    }  // namespace entity
+}  // namespace pixel_engine
