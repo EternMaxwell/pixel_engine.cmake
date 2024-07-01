7434e98d4e0fa936b7c1b2306d990d12a85eed6a
Etern Maxwell <maxwell_dev@outlook.com>
2024-07-01 09:37:50 +0800

    seperate declaration and definition.

---
+++ pixel_engine/include/pixel_engine/transform/components.h
@@ -0,0 +1,45 @@
+﻿#include <glm/fwd.hpp>
+#include <glm/vec3.hpp>
+
+#include "pixel_engine/entity.h"
+
+namespace pixel_engine {
+    namespace transform {
+        struct Transform {
+            glm::vec3 translation = glm::vec3(0.0f);
+            glm::quat quaternion = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+            glm::vec3 scale = glm::vec3(1.0f);
+
+            void translate(const glm::vec3& translation);
+            void rotate(const glm::vec3& axis, float angle);
+            void rotate_x(float angle);
+            void rotate_y(float angle);
+            void rotate_z(float angle);
+            void scale_by(const glm::vec3& scale);
+            void scale_x(float scale);
+            void scale_y(float scale);
+            void scale_z(float scale);
+            void get_matrix(glm::mat4* matrix) const;
+        };
+
+        struct Projection {
+            float fov = 45.0f;
+            float aspect = 1.0f;
+            float near = 0.1f;
+            float far = 100.0f;
+
+            void get_matrix(glm::mat4* matrix) const;
+        };
+
+        struct OrthoProjection {
+            float left = -1.0f;
+            float right = 1.0f;
+            float bottom = -1.0f;
+            float top = 1.0f;
+            float near = -1.0f;
+            float far = 1.0f;
+
+            void get_matrix(glm::mat4* matrix) const;
+        };
+    }  // namespace transform
+}  // namespace pixel_engine
---
+++ pixel_engine/src/pixel_engine/transform.cpp
@@ -0,0 +1,34 @@
+﻿#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+#include "pixel_engine/transform/components.h"
+
+void pixel_engine::transform::Transform::translate(const glm::vec3& translation) { this->translation += translation; }
+void pixel_engine::transform::Transform::rotate(const glm::vec3& axis, float angle) {
+    quaternion = glm::rotate(quaternion, angle, axis);
+}
+void pixel_engine::transform::Transform::rotate_x(float angle) {
+    quaternion = glm::rotate(quaternion, angle, glm::vec3(1.0f, 0.0f, 0.0f));
+}
+void pixel_engine::transform::Transform::rotate_y(float angle) {
+    quaternion = glm::rotate(quaternion, angle, glm::vec3(0.0f, 1.0f, 0.0f));
+}
+void pixel_engine::transform::Transform::rotate_z(float angle) {
+    quaternion = glm::rotate(quaternion, angle, glm::vec3(0.0f, 0.0f, 1.0f));
+}
+void pixel_engine::transform::Transform::scale_by(const glm::vec3& scale) { this->scale *= scale; }
+void pixel_engine::transform::Transform::scale_x(float scale) { this->scale.x *= scale; }
+void pixel_engine::transform::Transform::scale_y(float scale) { this->scale.y *= scale; }
+void pixel_engine::transform::Transform::scale_z(float scale) { this->scale.z *= scale; }
+void pixel_engine::transform::Transform::get_matrix(glm::mat4* matrix) const {
+    *matrix =
+        glm::translate(glm::mat4(1.0f), translation) * glm::mat4_cast(quaternion) * glm::scale(glm::mat4(1.0f), scale);
+}
+
+void pixel_engine::transform::Projection::get_matrix(glm::mat4* matrix) const {
+    *matrix = glm::perspective(fov, aspect, near, far);
+}
+
+void pixel_engine::transform::OrthoProjection::get_matrix(glm::mat4* matrix) const {
+    *matrix = glm::ortho(left, right, bottom, top, near, far);
+}
