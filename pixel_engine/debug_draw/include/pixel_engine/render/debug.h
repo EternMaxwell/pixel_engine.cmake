f87c4b7f2d6604635684d4aadff8ddd0b07995d0
Etern Maxwell <maxwell_dev@outlook.com>
2024-11-14 17:49:31 +0800

    triangle drawer.

--- pixel_engine/debug_draw/include/pixel_engine/render/debug.h
+++ pixel_engine/debug_draw/include/pixel_engine/render/debug.h
@@ -10,8 +10,10 @@ struct DebugRenderPlugin : Plugin {
     void build(App& app) override {
         app.add_system(Startup, systems::create_line_drawer);
         app.add_system(Startup, systems::create_point_drawer);
+        app.add_system(Startup, systems::create_triangle_drawer);
         app.add_system(Exit, systems::destroy_line_drawer);
         app.add_system(Exit, systems::destroy_point_drawer);
+        app.add_system(Exit, systems::destroy_triangle_drawer);
     }
 };
 }  // namespace vulkan
